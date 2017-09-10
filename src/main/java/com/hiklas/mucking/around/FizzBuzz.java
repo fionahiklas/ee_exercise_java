e94729e976f788fb228cb08d0bddc38b5701e8c7
Fiona Bianchi <fiona@hiklas.co.uk>
2017-09-10 18:37:08 +0100

    Added more tests and functionality.  Also handling zero modulus

--- src/main/java/com/hiklas/mucking/around/FizzBuzz.java
+++ src/main/java/com/hiklas/mucking/around/FizzBuzz.java
@@ -6,19 +6,36 @@ import java.util.function.Function;
 
 public class FizzBuzz
 {
+    public static final String FIZZ="fizz";
+    public static final String BUZZ="buzz";
 
     protected Function<Integer, Optional<String>> divideByThreeFunction()
     {
         return (numberToTransform) -> {
-            return Optional.empty();
+            if ( checkModulusOrZero(numberToTransform, 3)) return Optional.of(FIZZ);
+            else return Optional.empty();
         };
     }
 
     protected Function<Integer, Optional<String>> divideByFiveFunction()
     {
         return (numberToTransform) -> {
-            return Optional.empty();
+            if ( checkModulusOrZero(numberToTransform, 5)) return Optional.of(FIZZ);
+            else return Optional.empty();
         };
     }
 
+    /**
+     * Check if the numberToTest can be divided equally by the divisor,
+     * if it can return true.  If numberToTest is zero or can't be
+     * divided equally by the divisor return false
+     *
+     * @param numberToTest
+     * @param divisor
+     * @return
+     */
+    private boolean checkModulusOrZero(Integer numberToTest, Integer divisor)
+    {
+       return (numberToTest != 0 && numberToTest % divisor == 0);
+    }
 }
--- src/test/java/com/hiklas/mucking/around/FizzBuzzTest.java
+++ src/test/java/com/hiklas/mucking/around/FizzBuzzTest.java
@@ -3,11 +3,14 @@ package com.hiklas.mucking.around;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.equalTo;
+
 
 public class FizzBuzzTest
 {
@@ -37,4 +40,48 @@ public class FizzBuzzTest
         assertThat(divideByFive, notNullValue());
         assertThat(divideByFive, instanceOf(Function.class));
     }
+
+    @Test
+    public void divideByThreeFunction_returnsEmptyForZero()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByThreeFunction().apply(0);
+        assertThat(result.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void divideByFiveFunction_returnsEmptyForZero()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByFiveFunction().apply(0);
+        assertThat(result.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void divideByThreeFunction_returnsEmptyForOne()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByThreeFunction().apply(1);
+        assertThat(result.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void divideByFiveFunction_returnsEmptyForOne()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByFiveFunction().apply(1);
+        assertThat(result.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void divideByThreeFunction_returnsNonEmptyForThree()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByThreeFunction().apply(3);
+        assertThat(result.isPresent(), equalTo(true));
+    }
+
+    @Test
+    public void divideByFiveFunction_returnsNoneEmptyForFive()
+    {
+        Optional<String> result = fizzBuzzToTest.divideByFiveFunction().apply(5);
+        assertThat(result.isPresent(), equalTo(true));
+    }
+
+
 }
